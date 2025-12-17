6c5c9d495e5b5c9bbc0ce8b6b4d0dca8a6d44dba
Oguz-balik <oguz.balik@std.izmirekonomi.edu.tr>
2025-12-17 18:56:29 +0300

    Commit For Project

--- Main.java
+++ Main.java
@@ -2,226 +2,47 @@
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main {
     static final int MONTHS = 12;
     static final int DAYS = 28;
     static final int COMMS = 5;
     static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
     static String[] months = {"January","February","March","April","May","June",
-            "July","August","September","October","November","December"};
+                              "July","August","September","October","November","December"};
     static int[][][] data = new int[MONTHS][DAYS][COMMS];
     static int[][] mntlydata= new int[MONTHS][COMMS];
 
 
     // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
     public static void loadData() {
-        Scanner sc1 = null;
-        //JANUARY
         try {
-            sc1 = new Scanner(Paths.get("January.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[0][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[0][i]+=data[0][j][i];
+            for (int p = 0; p < MONTHS; p++) {
+                Scanner sc = new Scanner(Paths.get("src/Data_Files/" + months[p] + ".txt"));
+                while (sc.hasNextLine()) {
+                    String[] info = sc.nextLine().split(",");
+
+                    int day = Integer.parseInt(info[0]) - 1;
+                    String comm = info[1];
+                    int profit = Integer.parseInt(info[2]);
+
+                    for (int i = 0; i < COMMS; i++) {
+                        if (comm.equals(commodities[i])) {
+                            data[p][day][i] = profit;
+                            mntlydata[p][i] += profit;
+                            break;
                         }
                     }
                 }
+                sc.close();
             }
-        } catch (IOException e){}
-
-        //FEBRUARY
-
-        try {
-            sc1 = new Scanner(Paths.get("February.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[1][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[1][i]+=data[1][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-        //MARCH
-        try {
-            sc1 = new Scanner(Paths.get("March.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[2][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[2][i]+=data[2][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-
-
-        //APRİL
-
-        try {
-            sc1 = new Scanner(Paths.get("April.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[3][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[3][i]+=data[3][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-
-        //MAY
-
-        try {
-            sc1 = new Scanner(Paths.get("May.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[4][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[4][i]+=data[4][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-        //JUNE
-
-        try {
-            sc1 = new Scanner(Paths.get("June.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[5][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[5][i]+=data[5][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-        //JULY
-
-        try {
-            sc1 = new Scanner(Paths.get("July.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[6][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[6][i]+=data[6][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-        //AUGUST
-        try {
-            sc1 = new Scanner(Paths.get("August.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[7][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[7][i]+=data[7][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-
-        //SEPTEMBER
-        try {
-            sc1 = new Scanner(Paths.get("September.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[8][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[8][i]+=data[8][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-
-        //OCTOBER
-
-        try {
-            sc1 = new Scanner(Paths.get("October.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[9][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[9][i]+=data[9][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-
-        //NOVEMBER
-
-        try {
-            sc1 = new Scanner(Paths.get("November.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[10][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[10][i]+=data[10][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
-
-        //DECEMBER
-
-        try {
-            sc1 = new Scanner(Paths.get("December.txt"));
-            while(sc1.hasNextLine()) {
-                String[] info0 = sc1.nextLine().split(",");
-                for (int j=0;j<28;j++){
-                    for (int i=0;i<5;i++){
-                        if (info0[1].equals(commodities[i])){
-                            data[11][j][i]=Integer.parseInt(info0[2]);
-                            mntlydata[11][i]+=data[11][j][i];
-                        }
-                    }
-                }
-            }
-        } catch (IOException e){}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     // ======== 10 REQUIRED METHODS (Students fill these) ========
 
@@ -229,16 +50,16 @@ public class Main {
         if (month < 0 || month >= 12) {
             return "INVALID_MONTH";
         }
-        int maxProfit = 0;
+        int maxProfit = -99999;
         String mostProfitableComm = "";
-        for (int i=0;i<28;i++){
-            for (int j=0;j<5;j++){
-                if (data[month][i][j]>maxProfit) {
-                    maxProfit=data[month][i][j];
-                    mostProfitableComm=commodities[j];
+            for (int j = 0; j < COMMS; j++) {
+                if (mntlydata[month][j] > maxProfit) {
+                    maxProfit = mntlydata[month][j];
+                    mostProfitableComm = commodities[j];
                 }
             }
-        }
+
+
         return mostProfitableComm + " " + maxProfit;
     }
 
@@ -250,17 +71,50 @@ public class Main {
         else {
             int totalprofit=0;
             for (int j=0;j<5;j++){
-                totalprofit+=data[month][day][j];
+                totalprofit+=data[month][day-1][j];
             }
             return totalprofit;
         }}
 
     public static int commodityProfitInRange(String commodity, int from, int to) {
-        return 1234;
+     int profit = 0;
+     boolean cboo=false;
+     for (int i=0;i<5;i++){
+         if (commodity.equals(commodities[i])){
+             cboo=true;
+             break;
+         }
+     }
+     if (!cboo) return -99999;
+     if (from>to) return -99999;
+     else if (from<1) return -99999;
+     else if (to>28) return -99999;
+     else {for (int i=0;i<5;i++){
+        if (commodity.equals(commodities[i])){
+            for (int j=from-1;j<to;j++){
+                profit+=data[1][j][i];
+            }
+            break;
+        }
+     }
+     }
+     return profit;
     }
 
     public static int bestDayOfMonth(int month) {
-        return 1234;
+        if (month<0||month>11) return -1;
+        int bestday=-99999;
+        int bestdaynum=0;
+        for (int i=0;i<DAYS;i++){
+            int soac=0;
+            for (int j=0;j<COMMS;j++){
+                soac+=data[month][i][j];
+            }
+            if (soac>bestday) {bestday=soac;
+            bestdaynum=i;
+            }
+        }
+        return bestdaynum+1;
     }
 
     public static String bestMonthForCommodity(String comm) {
